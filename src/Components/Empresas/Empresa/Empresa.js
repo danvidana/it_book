6d63a9ce3ad4f06cb459ddd591df7bd9a092a536
BrunoMendez <bruno.m.275@gmail.com>
2021-04-07 19:28:51 -0500

    fetch de empresas

--- src/Components/Empresas/Empresa/Empresa.js
+++ src/Components/Empresas/Empresa/Empresa.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const Empresa = (props) => {
-	return <div>{props.empresa.nombre}</div>;
+	return (
+		<Card>
+			<Card.Body>
+				<Card.Title>{props.nombre}</Card.Title>
+				<Card.Text>
+					This is a wider card with supporting text below as a natural
+					lead-in to additional content. This content is a little bit
+					longer.
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default Empresa;
